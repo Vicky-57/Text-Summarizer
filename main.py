ab7138a787a8c5572113e1bbcb96a5f1552a4ec0
Vicky <vikaspurohit105>
2024-06-11 16:36:03 +0530

    data validationcompleted

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from Text_summarizer.pipeline.stage01_datainjestion import DataIngestionTrainingPipeline
 from Text_summarizer.logging import logger
-
+from Text_summarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/Text_summarizer/config/configuration.py
+++ src/Text_summarizer/config/configuration.py
@@ -1,7 +1,7 @@
 #UPDATING CONFIGURATION MANAGER IN SRC
 from Text_summarizer.constants import *
 from Text_summarizer.utilities.common import read_yaml, create_directories, get_size
-from Text_summarizer.entity import (DataIngestionConfig)
+from Text_summarizer.entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -29,3 +29,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
