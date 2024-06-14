9486da34b2ff86a8688333d163038f4575a4fe57
VIKAS DOLATSINH PUROHIT <210303105587@pu.edu>
2024-06-14 16:51:59 +0530

    data transformation notebook added

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open("README.md", "r",encoding="utf-8") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
+REPO_NAME = "Text-Summarizer"
 AUTHOR_USER_NAME = "Vicky-57"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "vikaspurohit105@gmail.com"
@@ -18,7 +18,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
