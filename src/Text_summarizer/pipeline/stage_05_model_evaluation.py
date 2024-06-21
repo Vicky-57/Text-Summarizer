be8acfa496f0e83bd2519408cf9e9534364823f0
vikas-57 <vikaspurohit105@gmail.com>
2024-06-21 15:41:51 +0530

    model evatuation complete

---
+++ src/Text_summarizer/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,16 @@
+from Text_summarizer.config.configuration import ConfigurationManager
+from Text_summarizer.components.model_evaluation import ModelEvaluation
+from Text_summarizer.logging import logger
+
+
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
